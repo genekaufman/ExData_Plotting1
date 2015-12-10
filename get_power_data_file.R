ec4950b2621993a9ae9934c5677231107f2bb82b
Gene Kaufman <genekaufman@gmail.com>
2015-12-10 09:10:24 -0500

    download zip file if necessary, add status messages

--- get_power_data_file.R
+++ get_power_data_file.R
@@ -1,15 +1,36 @@
-library(sqldf)
 library(dplyr)
-#download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","power_consumption.zip",mode="wb")
-unzip("power_consumption.zip")
-#mydata<-read.table("household_power_consumption.txt",header = TRUE,sep=";")
+library(sqldf)
+
+# Retrieve and unzip the data file if we don't have it already
+zip.file.url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+zip.file.local<-"power_consumption.zip"
+if (!file.exists(zip.file.local)) {
+  message("Downloading zip file")
+  download.file(zip.file.url,zip.file.local,mode="wb")
+}
+if (!file.exists(zip.file.local)) {
+  stop("Failed to retrieve zip file!")
+}
+message("Unzipping data file")
+unzip(zip.file.local)
+
+# When reading the data file, only keep the records that we're interested in
 rawfile<-"household_power_consumption.txt"
+message("Reading data file")
 mydata<-read.csv.sql(rawfile,sql="select * from file where Date in ('1/2/2007','2/2/2007')",header=TRUE,sep=";")
 closeAllConnections()
-unlink("household_power_consumption.txt")
+
+# Convert data frame to table frame for dplyr;
+message("Preparing data file")
 mydf<-tbl_df(mydata) %>%
-#mutate(Date = as.Date(Date,format="%d/%m/%Y") )%>%
-mutate(DateTime = as.POSIXct(strptime(paste(Date,Time),format="%d/%m/%Y %H:%M:%S")))
-#transmute(DateTime = as.POSIXct(strptime(paste(Date,Time),format="%d/%m/%Y %H:%M:%S")))
+  # The 'Date' and 'Time' fields are characters, convert them to a single Time field
+  mutate(DateTime = as.POSIXct(strptime(paste(Date,Time),format="%d/%m/%Y %H:%M:%S")))
+
+# Clean up unnecessary files and variables
+message("Cleaning temp items")
+unlink("household_power_consumption.txt")
 rm(mydata)
 rm(rawfile)
+rm(zip.file.url)
+rm(zip.file.local)
+message("Data loaded, ready for processing")
